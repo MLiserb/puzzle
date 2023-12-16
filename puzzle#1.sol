cdaf44a9af2127bd28d7ee7c0f6efc1c4892188c
Marc <144083324+MLiserb@users.noreply.github.com>
2023-12-16 15:38:52 +0800

    Update puzzle#1.sol

--- puzzle#1.sol
+++ puzzle#1.sol
@@ -1,67 +1,159 @@
-// We use this to keep track of which indices [0, 63] have been filled.
-uint256 bitmap = 1 << 64;
-// The bitmap only reserves bits in positions [0, 63] to represent the slots
-// that have been filled. Thus, 64 is a sentinel value that will always yield 1
-// when retrieved via `(bitmap >> index) & 1`, and it can be used as a sensible
-// default value before the next iteration of the `for` loop.
-uint256 index = 64;
-// We fill the puzzle randomly with 1 of [1, 8].
-for (uint256 i = 1; i < 9; ) {
-    // We have exhausted the seed, so stop iterating.
-    if (seed == 0) break;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-    // Loop through until we find an unfilled index.
-    while ((bitmap >> index) & 1 == 1 && seed != 0) {
-        // Retrieve 6 random bits from `seed` to determine which index to fill.
-        index = seed & 0x3f;
-        seed >>= 6;
-    }
-    // Set the bit in the bitmap to indicate that the index has been filled.
-    bitmap |= 1 << index;
+contract Sudoku {
+    uint256 public CHECKS;
+    uint256 public ROW_SHIFTS[8];
+    uint256 public COL_SHIFTS[8];
+    uint256 public SUBGRID_SHIFTS[8];
 
-    // Place the number into the slot that was just filled.
-    puzzle |= (i << (index << 2));
-    index = 64;
-    unchecked {
-        ++i;
+    uint256 public puzzle;
+    uint256 public solution;
+
+    constructor() {
+        // Initialize CHECKS with pre-calculated values for each index.
+        CHECKS = 0b1010101010101010101010101010101010101010101010101010101010101010;
+
+        ROW_SHIFTS[0] = 0 * 4;
+        ROW_SHIFTS[1] = 1 * 4;
+        ROW_SHIFTS[2] = 2 * 4;
+        ROW_SHIFTS[3] = 3 * 4;
+        ROW_SHIFTS[4] = 4 * 4;
+        ROW_SHIFTS[5] = 5 * 4;
+        ROW_SHIFTS[6] = 6 * 4;
+        ROW_SHIFTS[7] = 7 * 4;
+
+        COL_SHIFTS[0] = 0;
+        COL_SHIFTS[1] = 1;
+        COL_SHIFTS[2] = 2;
+        COL_SHIFTS[3] = 3;
+        COL_SHIFTS[4] = 4;
+        COL_SHIFTS[5] = 5;
+        COL_SHIFTS[6] = 6;
+        COL_SHIFTS[7] = 7;
+
+        SUBGRID_SHIFTS[0] = 0 * 8;
+        SUBGRID_SHIFTS[1] = 1 * 8;
+        SUBGRID_SHIFTS[2] = 2 * 8;
+        SUBGRID_SHIFTS[3] = 3 * 8;
+        SUBGRID_SHIFTS[4] = 0 * 8;
+        SUBGRID_SHIFTS[5] = 1 * 8;
+        SUBGRID_SHIFTS[6] = 2 * 8;
+        SUBGRID_SHIFTS[7] = 3 * 8;
     }
-}
-// Check rows, columns, and subgrids for Sudoku rule violations.
-for (uint256 index = 0; index < 64; index++) {
-    uint256 checks = (CHECKS >> index) & 7;
-    if (checks & 4 == 4) {
-        if (!check(_solution, ROW_SHIFTS)) {
-            return false;
+
+    function generatePuzzle(uint256[] memory seed) private {
+        uint256 bitmap = 1 << 64;
+        uint256 index = 64;
+        uint256 unpacked_seed;
+
+        for (uint256 i = 0; i < 64; i++) {
+            if (seed[i / 16] & (1 << (i % 16)) == 0) {
+                unpacked_seed = seed[i / 16];
+                break;
+            }
+
+            unpacked_seed = seed[i / 16];
+            unpacked_seed = (unpacked_seed << 6) | (seed[i / 16] >> (i % 16));
+        }
+
+        for (uint256 i = 0; i < 8; i++) {
+            for (uint256 j = 0; j < 8; j++) {
+                if (i == 0) {
+                    for (uint256 k = 0; k < 8; k++) {
+                        for (uint256 l = 0; l < 8; l++) {
+                            uint256 shift = (i * 8 + j) * 4 + (k * 8 + l);
+                            uint256 check = 1 << shift;
+                            if ((CHECKS >> shift) & 1 == 1) {
+                                unchecked {
+                                    puzzle |= (check << (shift << 2));
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    for (uint256 k = 0; k < 8; k++) {
+                        uint256 shift = (i * 8 + j) * 4 + (k * 8 + l);
+                        uint256 check = 1 << shift;
+                        if ((CHECKS >> shift) & 1 == 1) {
+                            unchecked {
+                                puzzle |= (check << (shift << 2));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (uint256 i = 0; i < 64; i++) {
+            uint256 shift = i * 4;
+            uint256 check = 1 << shift;
+            if ((CHECKS >> shift) & 1 == 1) {
+                uint256 seed_part = unpacked_seed & (0xf << (i % 16) * 4);
+                uint256 value;
+
+                if (i < 8) {
+                    value = (seed_part >> (4 * (i % 8))) & 0xf;
+                } else {
+                    uint256 seed_shift =
+                        (seed_part >> (4 * ((i - 8) % 8))) & 0xf
+                        |
+                        (seed_part >> (4 * (((i - 8) % 8) + 8))) & 0xf
+                        |
+                        (seed_part >> (4 * (((i - 8) % 8) + 8))) & 0xf;
+
+                    value = seed_shift;
+                }
+
+                uint256 puzzle_part = puzzle >> shift & 0xf;
+
+                if (puzzle_part != 0 && puzzle_part != value) {
+                    return;
+                }
+
+                if (value != 0) {
+                    puzzle |= check;
+                }
+            }
         }
     }
-    if (checks & 2 == 2) {
-        if (!check(_solution, COL_SHIFTS)) {
-            return false;
+
+    function verifySolution(
+        uint256[] memory solution,
+        uint256[] memory shifts
+    ) private pure returns (bool) {
+        // ... (the existing part)
+
+        for (uint256 index = 0; index < 64; index += 4) {
+            uint256 checks = (CHECKS >> index) & 7;
+
+            if (checks & 4 == 4) {
+                if (!check(_solution, shifts, ROW_SHIFTS)) {
+                    return false;
+                }
+            }
+
+            if (checks & 2 == 2) {
+                if (!check(_solution, shifts, COL_SHIFTS)) {
+                    return false;
+                }
+            }
+
+            if (checks & 1 == 1) {
+                if (!check(_solution, shifts, SUBGRID_SHIFTS)) {
+                    return false;
+                }
+            }
         }
-    }
-    if (checks & 1 == 1) {
-        if (!check(_solution, SUBGRID_SHIFTS)) {
-            return false;
-        }
-    }
-}
 
-// If all the checks pass at every index of the board, we have a valid solution!
-return true;
-
-function check(uint256 memory values, uint256[8] memory shifts) private pure returns (bool) {
-    uint256[8] memory counts = new uint256[8](8);
-
-    for (uint256 i = 0; i < 8; i++) {
-        uint256 index = values >> shifts[i] & 0xf;
-        counts[index] += 1;
+        return true;
     }
 
-    for (uint256 i = 0; i < 8; i++) {
-        if (counts[i] != 1) {
-            return false;
-        }
+    function check(
+        uint256 memory solution,
+        uint256[] memory shifts,
+        uint256[] memory subgridShifts
+    ) private pure returns (bool) {
+        // ... (the existing check function)
     }
-
-    return true;
 }
